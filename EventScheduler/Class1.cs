a110c06a7b3653daf7dd02337befb1a16f13690b
Sphexator <kristoffer.tunlid@gmail.com>
2018-09-19 22:23:20 +0200

    initialize

--- EventScheduler/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EventScheduler
-{
-    public class Class1
-    {
-    }
-}
---
+++ EventScheduler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EventScheduler
+{
+    internal class Program
+    {
+        private static void Main() => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            Directory.CreateDirectory("Data");
+            while (true)
+            {
+                var watcher = new FileSystemWatcher
+                {
+                    Path = @"Data/",
+                    NotifyFilter = NotifyFilters.LastWrite,
+                    EnableRaisingEvents = true,
+                    Filter = "*.*"
+                };
+                watcher.Changed += OnChanged;
+
+                await Task.Delay(TimeSpan.FromHours(1));
+            }
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            if ((e.ChangeType & WatcherChangeTypes.Created) != 0) return;
+            
+        }
+    }
+}
